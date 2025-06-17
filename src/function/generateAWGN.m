7ffb8977cba811a858e74d8f0061fd53e29a460d
kth0512 <kth0512@postech.ac.kr>
2025-06-17 13:52:26 +0900

    Update generateAWGN.m

--- src/function/generateAWGN.m
+++ src/function/generateAWGN.m
@@ -1,5 +1,5 @@
 % precondition: length > 0 && N0 >= 0
-% postcondition: noiseArray is 1xlength complex row vector such that each element v is AWGN with variance N0. i.e.) v ~ 𝒞𝒩(0, N0)
+% postcondition: noiseArray is a 1xlength complex row vector such that each element v is AWGN with variance N0. i.e.) v ~ 𝒞𝒩(0, N0)
 function noiseArray = generateAWGN(N0, length)  
     realPart = normrnd(0, sqrt(N0/2), [1, length]);
     imaginaryPart = normrnd(0, sqrt(N0/2), [1, length]);
