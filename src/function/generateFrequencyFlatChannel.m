51f720f1712873d775666271bf644477674121fa
kth0512 <kth0512@postech.ac.kr>
2025-06-17 13:45:19 +0900

    Update generateFrequencyFlatChannel.m
    comment: add pre/postconditions

--- src/function/generateFrequencyFlatChannel.m
+++ src/function/generateFrequencyFlatChannel.m
@@ -1,3 +1,5 @@
+% precondition: true
+% postcondition: frequencyFlatChannel is a 1x1 complex scalar h such that h ~ 𝒞𝒩(0, 1) 
 function frequencyFlatChannel = generateFrequencyFlatChannel
     realPartOfChannel = normrnd(0, sqrt(1/2));
     imaginaryPartOfChannel = normrnd(0, sqrt(1/2));
