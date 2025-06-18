eb80b1e0feca98132f601d44f3a110b30aa57491
kth0512 <kth0512@postech.ac.kr>
2025-06-18 15:33:00 +0900

    Update Week3_FreqFlat.m
    Update plot styling for clarity

--- src/Week3_FreqFlat.m
+++ src/Week3_FreqFlat.m
@@ -37,14 +37,14 @@ disp(SNR_BER)
 figure;
 hold on;
 grid on;
-semilogy(SNR_BER(:, 1), SNR_BER(:, 2), '-o', 'MarkerEdgeColor', 'b', 'DisplayName', 'Ntr = 2');
-semilogy(SNR_BER(:, 3), SNR_BER(:, 4), '-o', 'MarkerEdgeColor', 'r', 'DisplayName', 'Ntr = 5');
-semilogy(SNR_BER(:, 5), SNR_BER(:, 6), '-o', 'MarkerEdgeColor', 'g', 'DisplayName', 'Ntr = 10');
+semilogy(SNR_BER(:, 1), SNR_BER(:, 2), '-o', 'LineWidth', 1.5, 'Color', [0, 0.4470, 0.7410], 'MarkerEdgeColor', [0, 0.4470, 0.7410], 'DisplayName', 'Ntr = 2');
+semilogy(SNR_BER(:, 3), SNR_BER(:, 4), '-^', 'LineWidth', 1.5, 'Color', [0.4940, 0.1840, 0.5560], 'MarkerEdgeColor', [0.4940, 0.1840, 0.5560], 'DisplayName', 'Ntr = 5');
+semilogy(SNR_BER(:, 5), SNR_BER(:, 6), '-s', 'LineWidth', 1.5, 'Color', [0.4660, 0.6740, 0.1880], 'MarkerEdgeColor', [0.4660, 0.6740, 0.1880], 'DisplayName', 'Ntr = 10');
 set(gca, 'YScale', 'log');
 xlabel('SNR(dB)')
 ylabel('BER')
 title('BER VS SNR Curve');
 legend('Location', 'best');
-ylim([10^(-8), 1])
+ylim([10^(-5), 1])
 hold off;
 
