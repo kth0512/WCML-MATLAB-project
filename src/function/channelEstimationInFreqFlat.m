34d3e504a8738587c7aa16057d9df79a28cf42ac
kth0512 <kth0512@postech.ac.kr>
2025-06-19 13:29:21 +0900

    Update channelEstimationInFreqFlat.m

--- src/function/channelEstimationInFreqFlat.m
+++ src/function/channelEstimationInFreqFlat.m
@@ -1,5 +1,3 @@
-% precondition: trainingSequence and receivedSingal are row vector with same dimesion.
-% postcondition: estimatedChannel is 1x1 complex scalar h_hat determined by least square method.
 function estimatedChannel = channelEstimationInFreqFlat(filter, receivedSignal)
     estimatedChannel = receivedSignal*filter;
 end
