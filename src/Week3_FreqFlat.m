79914fe57d3ef21269e085e9094069fab05ae0a3
kth0512 <kth0512@postech.ac.kr>
2025-06-19 13:23:24 +0900

    Update Week3_FreqFlat.m

--- src/Week3_FreqFlat.m
+++ src/Week3_FreqFlat.m
@@ -7,14 +7,17 @@ M = 16;
 lengthOfBitSequence = 4*100;
 numOfSymbol = 100;
 SNR_BER = zeros(41, 6);
+SNR_BER_LMMSE = zeros(41, 6);
 
 for i = 1:numOfNtr
     Ntr = ListOfNtr(i);
     zadoffChuTrainingSequence = generateZadoffChuTrainingSequence(1, Ntr);
-    filter = (zadoffChuTrainingSequence')/(zadoffChuTrainingSequence*zadoffChuTrainingSequence');
+    filterForLS = (zadoffChuTrainingSequence')/(zadoffChuTrainingSequence*zadoffChuTrainingSequence');
+    filterForLMMSE = (zadoffChuTrainingSequence')/(zadoffChuTrainingSequence*zadoffChuTrainingSequence' + N0);
     for j = 0:40 
         Ex = 10^(j/10);
-        sumOfBER = 0;
+        sumOfBERLS = 0;
+        sumOfBERLMMSE = 0;
         for k = 1:numOfIteration
             h = generateFrequencyFlatChannel;
             bitSequence = generateRandomBitSequence(lengthOfBitSequence);
@@ -23,18 +26,35 @@ for i = 1:numOfNtr
             symbolWithTraining = [zadoffChuTrainingSequence s];
             y = generateReceivedSignalInFreqFlat(symbolWithTraining, v, Ex, h);
             yTraining = y(1:Ntr);
-            hHat = channelEstimationInFreqFlatWithLS(filter, yTraining); % zadoffChuTrainingSequence is not multiplied with sqrt(Ex)
+
+            % Least Square (LS)
+            hHat = channelEstimationInFreqFlat(filterForLS, yTraining); % zadoffChuTrainingSequence is not multiplied with sqrt(Ex)
             yEq = equalizeFreqFlatChannel(y, hHat);
             sHat = detectSymbolsWithML(yEq(Ntr+1:numOfSymbol+Ntr), M, 1);
             estimatedBitSequence = mapSymbolsToBits(sHat, M);
             BER = calculateBER(bitSequence, estimatedBitSequence);
             sumOfBER = sumOfBER + BER;
+
+            % Linear Minimum Mean Square Error (LMMSE)
+            hHatLMMSE = channelEstimationInFreqFlat(filterForLMMSE, yTraining); % zadoffChuTrainingSequence is not multiplied with sqrt(Ex)
+            yEqLMMSE = equalizeFreqFlatChannel(y, hHatLMMSE);
+            sHatLMMSE = detectSymbolsWithML(yEqLMMSE(Ntr+1:numOfSymbol+Ntr), M, 1);
+            estimatedBitSequenceLMMSE = mapSymbolsToBits(sHatLMMSE, M);
+            BERLMMSE = calculateBER(bitSequence, estimatedBitSequenceLMMSE);
+            sumOfBERLMMSE = sumOfBERLMMSE + BERLMMSE;
+
+
         end
+        % Least Square
         averageOfBER = sumOfBER/numOfIteration;
         SNR_BER(j+1, 2*i-1:2*i) = [j, averageOfBER];
+
+        % Linear Minimum Mean Square Error
+        averageOfBERLMMSE = sumOfBERLMMSE/numOfIteration;
+        SNR_BER_LMMSE(j+1, 2*i-1:2*i) = [j, averageOfBERLMMSE];
     end
 end
-disp(SNR_BER)
+
 figure;
 hold on;
 grid on;
@@ -49,3 +69,16 @@ legend('Location', 'best');
 ylim([10^(-5), 1])
 hold off;
 
+figure;
+hold on;
+grid on;
+semilogy(SNR_BER_LMMSE(:, 1), SNR_BER_LMMSE(:, 2), '--o', 'LineWidth', 1.5, 'Color', [0, 0.4470, 0.7410], 'MarkerEdgeColor', [0, 0.4470, 0.7410], 'DisplayName', 'Ntr = 2');
+semilogy(SNR_BER_LMMSE(:, 3), SNR_BER_LMMSE(:, 4), '--^', 'LineWidth', 1.5, 'Color', [0.4940, 0.1840, 0.5560], 'MarkerEdgeColor', [0.4940, 0.1840, 0.5560], 'DisplayName', 'Ntr = 5');
+semilogy(SNR_BER_LMMSE(:, 5), SNR_BER_LMMSE(:, 6), '--s', 'LineWidth', 1.5, 'Color', [0.4660, 0.6740, 0.1880], 'MarkerEdgeColor', [0.4660, 0.6740, 0.1880], 'DisplayName', 'Ntr = 10');
+set(gca, 'YScale', 'log');
+xlabel('SNR(dB)')
+ylabel('BER')
+title('BER VS SNR Curve');
+legend('Location', 'best');
+ylim([10^(-5), 1])
+hold off;
