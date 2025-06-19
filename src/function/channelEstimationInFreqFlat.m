b43848f5b058b3bba5d59f498e99814bb17604ae
kth0512 <kth0512@postech.ac.kr>
2025-06-19 13:23:14 +0900

    Update and rename channelEstimationInFreqFlatWithLS.m to channelEstimationInFreqFlat.m

--- src/function/channelEstimationInFreqFlat.m
+++ src/function/channelEstimationInFreqFlat.m
@@ -1,5 +1,5 @@
 % precondition: trainingSequence and receivedSingal are row vector with same dimesion.
 % postcondition: estimatedChannel is 1x1 complex scalar h_hat determined by least square method.
-function estimatedChannel = channelEstimationInFreqFlatWithLS(filter, receivedSignal)
+function estimatedChannel = channelEstimationInFreqFlat(filter, receivedSignal)
     estimatedChannel = receivedSignal*filter;
 end
