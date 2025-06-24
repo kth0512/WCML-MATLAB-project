d3e2276ee73e563049249b2a8168a2d139d6a6d4
kth0512 <kth0512@postech.ac.kr>
2025-06-24 00:13:48 -0400

    Update Week3_FreqFlat.m
    delete LMMSE and add theoretical value

--- src/Week3_FreqFlat.m
+++ src/Week3_FreqFlat.m
@@ -7,7 +7,6 @@ M = 16;
 lengthOfBitSequence = 4*100;
 numOfSymbol = 100;
 SNR_BER_LS = zeros(41, 6);
-SNR_BER_LMMSE = zeros(41, 6);
 
 for i = 1:numOfNtr
     Ntr = ListOfNtr(i);
@@ -16,8 +15,6 @@ for i = 1:numOfNtr
     for j = 0:40 
         N0 = 10^(-j/10);
         sumOfBERLS = 0;
-        sumOfBERLMMSE = 0;
-        filterForLMMSE = (t')/(t*t' + N0);
         for k = 1:numOfIteration
             h = generateFrequencyFlatChannel;
             bitSequence = generateRandomBitSequence(lengthOfBitSequence);
@@ -28,28 +25,16 @@ for i = 1:numOfNtr
             yTraining = y(1:Ntr);
 
             % Least Square (LS)
-            hHatLS = channelEstimationInFreqFlat(filterForLS, yTraining); % zadoffChuTrainingSequence is not multiplied with sqrt(Ex)
+            hHatLS = channelEstimationInFreqFlat(filterForLS, yTraining); 
             yEqLS = equalizeFreqFlatChannel(y, hHatLS);
             sHatLS = detectSymbolsWithML(yEqLS(Ntr+1:numOfSymbol+Ntr), M, Ex);
             estimatedBitSequenceLS = mapSymbolsToBits(sHatLS, M);
             BERLS = calculateBER(bitSequence, estimatedBitSequenceLS);
             sumOfBERLS = sumOfBERLS + BERLS;
-
-            % Linear Minimum Mean Square Error (LMMSE)
-            hHatLMMSE = channelEstimationInFreqFlat(filterForLMMSE, yTraining); % zadoffChuTrainingSequence is not multiplied with sqrt(Ex)
-            yEqLMMSE = equalizeFreqFlatChannel(y, hHatLMMSE);
-            sHatLMMSE = detectSymbolsWithML(yEqLMMSE(Ntr+1:numOfSymbol+Ntr), M, Ex);
-            estimatedBitSequenceLMMSE = mapSymbolsToBits(sHatLMMSE, M);
-            BERLMMSE = calculateBER(bitSequence, estimatedBitSequenceLMMSE);
-            sumOfBERLMMSE = sumOfBERLMMSE + BERLMMSE;
         end
         % Least Square
         averageOfBERLS = sumOfBERLS/numOfIteration;
         SNR_BER_LS(j+1, 2*i-1:2*i) = [j, averageOfBERLS];
-
-        % Linear Minimum Mean Square Error
-        averageOfBERLMMSE = sumOfBERLMMSE/numOfIteration;
-        SNR_BER_LMMSE(j+1, 2*i-1:2*i) = [j, averageOfBERLMMSE];
     end
 end
 
@@ -63,9 +48,18 @@ semilogy(SNR_BER_LS(:, 1), SNR_BER_LS(:, 2), '-o', 'LineWidth', 1.5, 'Color', [0
 semilogy(SNR_BER_LS(:, 3), SNR_BER_LS(:, 4), '-^', 'LineWidth', 1.5, 'Color', [0.4940, 0.1840, 0.5560], 'MarkerEdgeColor', [0.4940, 0.1840, 0.5560], 'DisplayName', 'Ntr = 5, LS');
 semilogy(SNR_BER_LS(:, 5), SNR_BER_LS(:, 6), '-s', 'LineWidth', 1.5, 'Color', [0.4660, 0.6740, 0.1880], 'MarkerEdgeColor', [0.4660, 0.6740, 0.1880], 'DisplayName', 'Ntr = 10, LS');
 
-semilogy(SNR_BER_LMMSE(:, 1), SNR_BER_LMMSE(:, 2), ':o', 'LineWidth', 1.5, 'Color', [0, 0.4470, 0.7410], 'MarkerEdgeColor', [0, 0.4470, 0.7410], 'DisplayName', 'Ntr = 2, LMMSE');
-semilogy(SNR_BER_LMMSE(:, 3), SNR_BER_LMMSE(:, 4), ':^', 'LineWidth', 1.5, 'Color', [0.4940, 0.1840, 0.5560], 'MarkerEdgeColor', [0.4940, 0.1840, 0.5560], 'DisplayName', 'Ntr = 5, LMMSE');
-semilogy(SNR_BER_LMMSE(:, 5), SNR_BER_LMMSE(:, 6), ':s', 'LineWidth', 1.5, 'Color', [0.4660, 0.6740, 0.1880], 'MarkerEdgeColor', [0.4660, 0.6740, 0.1880], 'DisplayName', 'Ntr = 10, LMMSE');
+% ideal value
+SNR_Range = 0:40;                             
+SNR_Linear   = 10.^(SNR_Range/10);
+
+theoreticalBPSK = 0.5 * (1 - sqrt(SNR_Linear ./ (1 + SNR_Linear)));
+semilogy(SNR_Range, theoreticalBPSK, 'k--', ...
+    'LineWidth',1.5, 'DisplayName','BPSK theory');
+
+theoreticalQPSK = 0.5 * (1 - sqrt(SNR_Linear ./ (2 + SNR_Linear)));
+semilogy(SNR_Range, theoreticalQPSK, 'm--', ...
+    'LineWidth',1.5, 'DisplayName','QPSK theory');
+
 
 set(gca, 'YScale', 'log');
 xlabel('SNR(dB)')
