88e11996b78f7bba6ed1ecab5b8c5b7a451ac0f9
kth0512 <kth0512@postech.ac.kr>
2025-06-25 14:49:15 +0900

    Add files via upload

---
+++ src/function/optimizeEqualizerDelay.m
@@ -0,0 +1,15 @@
+function ndOptimal = optimizeEqualizerDelay(H, Lf)
+    ndOptimal = 0;
+    Jmin = inf;
+    projectionMatrix = (H/(H'*H))*H';
+    I = eye(Lf);
+    for nd = 0:Lf-1
+        e = zeros(Lf,1);  
+        e(nd+1)=1;
+        J = norm((projectionMatrix - I)*e, 2)^2;
+        if (J < Jmin)
+            Jmin = J;
+            ndOptimal = nd;
+        end     
+    end
+end
