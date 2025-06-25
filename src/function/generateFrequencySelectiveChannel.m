cb9c1178c6e0f7a7f6cbf91b4a65075d16b6229d
kth0512 <kth0512@postech.ac.kr>
2025-06-25 10:48:23 +0900

    Update generateFrequencySelectiveChannel.m

--- src/function/generateFrequencySelectiveChannel.m
+++ src/function/generateFrequencySelectiveChannel.m
@@ -1,6 +1,6 @@
 function frequencySelectiveChannel = generateFrequencySelectiveChannel(numOfChannelTaps, variance)
     frequencySelectiveChannel = zeros(numOfChannelTaps);
     for i = 1:numOfChannelTaps
-        frequencySelectiveChannel(i) = generateFrequencyFlatChannel(1/3);
+        frequencySelectiveChannel(i) = generateFrequencyFlatChannel(variance);
     end
 end
