8cb86ab5684730e4b1aa949d7d94c735c772c1a4
kth0512 <kth0512@postech.ac.kr>
2025-06-25 15:34:42 +0900

    Update Week4_FreqSel.m

--- src/Week4_FreqSel.m
+++ src/Week4_FreqSel.m
@@ -1,36 +1,42 @@
 % simulation
 addpath('function/');
+
 Ntr = 10;
-numOfIteration = 1000;
 Ex = 1;
 M = 16;
+L = 3;
+
 lengthOfBitSequence = log2(M)*100;
+numOfIteration = 1000;
 numOfSymbol = 100;
 numOfSNR = 5;
-SNR_BER_LS = zeros(numOfSNR, 2);
-SNR_BER_LS(:, 1) = [10, 15, 20, 25, 30]; % [10, 15, 20, 25, 30] <- dB or not?
-L = 3;
+numOfLf = 2;
+
+SNRdB = [10, 15, 20, 25, 30]; % [10, 15, 20, 25, 30] <- dB or not?
+BER = zeros(numOfLf, numOfSNR);
+listOfLf = [5, 10];
 
 t = generateZadoffChuTrainingSequence(1, Ntr); % 1xNtr row vector
 T = generateTrainingMatrix(t, L);
-filter = (T'*T)\(T');
-
+filter = calculateLeftInverse(T);
 
 for i = 1:numOfSNR
-    %N0 = 10^(-SNR_BER_LS(i, 1)/10);
-    N0 = 0;
+    N0 = 10^(-SNRdB(i)/10);
     sumOfBER = 0;
     for j = 1:numOfIteration
         h = generateFrequencySelectiveChannel(3, 1/3);
-        b = generateRandomBitSequence(lengthOfBitSequence);
-        s = mapBitsToSymbols(b, M);
+        bitSequence = generateRandomBitSequence(lengthOfBitSequence);
+        s = mapBitsToSymbols(bitSequence, M);
         v = generateAWGN(N0, numOfSymbol+Ntr+L-1);
         t_s = [t s];
         y = generateReceivedSignalInFreqSel(t_s, v, h);
         yforChannelEst = y(L:Ntr);
         hEst = channelEstimation(filter, yforChannelEst.');
+        
+        for Lf = listOfLf
+            H = toeplitz([hEst ; zeros(Lf-1,1)],[hEst ; zeros(Lf-1,1)]);
 
-
+        end
        
     end
     
