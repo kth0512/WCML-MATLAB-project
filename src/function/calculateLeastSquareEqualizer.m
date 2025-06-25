7215816850bfc1cb1767d01b508d2a2532d71344
kth0512 <kth0512@postech.ac.kr>
2025-06-25 16:01:34 +0900

    Add files via upload

--- src/function/calculateLeastSquareEqualizer.m
+++ src/function/calculateLeastSquareEqualizer.m
@@ -1,5 +1,5 @@
-function leastSquareEqualizer = calculateLeastSquareEqualizer(H, nd)
-    e = zeros(Lf,1);  
+function leastSquareEqualizer = calculateLeastSquareEqualizer(H, nd, Lf, L)
+    e = zeros(Lf+L-1,1);  
     e(nd+1)=1;
     leastSquareEqualizer = calculateLeftInverse(H)*e;
 end
