01b6c754111ea3a149657142e499c3654112a224
kth0512 <kth0512@postech.ac.kr>
2025-06-26 13:21:30 +0900

    Update equalizeFreqSelChannel.m

--- src/function/equalizeFreqSelChannel.m
+++ src/function/equalizeFreqSelChannel.m
@@ -1,12 +1,13 @@
 function estimatedSymbols = equalizeFreqSelChannel(f, y, nd, numOfSymbol, Lf)
     lengthOfy = length(y);
     Y = zeros(numOfSymbol, Lf);
-    for i = 0:Lf-1
-        for j = 1:numOfSymbol
-            if 1 <= (-i+j+nd) && (-i+j+nd) <= lengthOfy
-                Y(j, i+1) = y(nd-i+j);
+    % 1-based
+    for i = 1:numOfSymbol
+        for j = 1:Lf
+            if 1 <= (nd+1+i-j) && (nd+1+i-j) <= lengthOfy
+                Y(i, j) = y(nd+1+i-j);
             else
-                Y(j, i+1) = 0;
+                Y(i, j) = 0;
             end
         end
     end
