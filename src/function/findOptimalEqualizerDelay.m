0d01595e33c1f56ec8b2782a73c62a6870d23718
kth0512 <kth0512@postech.ac.kr>
2025-06-26 13:07:11 +0900

    Update findOptimalEqualizerDelay.m

--- src/function/findOptimalEqualizerDelay.m
+++ src/function/findOptimalEqualizerDelay.m
@@ -1,15 +1,8 @@
 function ndOptimal = findOptimalEqualizerDelay(H, Lf, L)
-    ndOptimal = 0;
-    Jmin = inf;
-    I = eye(Lf+L-1);
-    projectionMatrix = H*calculateLeftInverse(H);
-    for nd = 0:Lf+L-2
-        e = zeros(Lf+L-1,1);  
-        e(nd+1)=1;
-        J = norm((projectionMatrix - I)*e, 2)^2;
-        if (J < Jmin)
-            Jmin = J;
-            ndOptimal = nd;
-        end     
-    end
+    I = eye(Lf + L - 1);                    
+    P = H * calculateLeftInverse(H);        
+    E = P - I;                              
+    columnNorms = vecnorm(E, 2, 1);         
+    [~, ndOptimal] = min(columnNorms);     
+    ndOptimal = ndOptimal - 1;              
 end
